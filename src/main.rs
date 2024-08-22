99b4b03812b97a6d83a2c1c343fdcfed04bfdce7
maytees <mateespublicprofile@gmail.com>
2024-08-21 20:25:41 -0400

    feat: basic controls and layout

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,70 @@ mod tui;
 
 use ratatui::{
     buffer::Buffer,
-    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    crossterm::event::{
+        self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers, ModifierKeyCode,
+    },
     layout::{Alignment, Rect},
     style::{Style, Stylize},
     symbols::border,
     text::{Line, Text},
     widgets::{
         block::{Position, Title},
-        Block, Paragraph, Widget,
+        Block, Padding, Paragraph, Widget,
     },
     Frame,
 };
 
-use color_eyre::{
-    eyre::{bail, WrapErr},
-    Result,
+use std::{
+    collections::HashSet,
+    fs::{self, File},
+    io::{BufRead, BufReader, Result},
 };
+
+use rand::thread_rng;
+use rand::{random, seq::SliceRandom};
 
 #[derive(Debug, Default)]
 pub struct App {
-    counter: i64,
+    target_word: String,
+    // 0,1,2,3, or 4 (basically the rows (just add 1 to index))
+    // Default: 0
+    round: u8,
+    reveal_answer: bool,
     exit: bool,
 }
 
 impl App {
     pub fn run(&mut self, terminal: &mut tui::Tui) -> Result<()> {
+        self.choose_random_word()
+            .expect("Could not get random word!");
+
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
-            self.handle_events().wrap_err("Handle events failed")?;
+            self.handle_events()?;
         }
         Ok(())
+    }
+
+    pub fn choose_random_word(&mut self) -> Result<()> {
+        let path = "fiveletterwords.txt";
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        let words: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+
+        if words.is_empty() {
+            eprintln!("The file is empty or cannot be read.");
+        }
+
+        let mut rng = rand::thread_rng();
+        if let Some(random_word) = words.choose(&mut rng) {
+            self.target_word = random_word.clone();
+            Ok(())
+        } else {
+            eprintln!("Could not get random word!");
+            Ok(())
+        }
     }
 
     fn render_frame(&self, frame: &mut Frame) {
@@ -39,53 +73,55 @@ impl App {
     }
 
     fn handle_events(&mut self) -> Result<()> {
-        match event::read()? {
+        let _ = match event::read()? {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
-            _ => {}
+            _ => Ok(()),
         };
 
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
         match key_event.code {
-            KeyCode::Char('q') | KeyCode::Char('Q') => self.exit(),
-            KeyCode::Char('j') => self.add_to_counter(-1),
-            KeyCode::Char('k') => self.add_to_counter(1),
-            KeyCode::Char('J') => self.add_to_counter(-10),
-            KeyCode::Char('K') => self.add_to_counter(10),
-            _ => {}
-        }
+            KeyCode::Char('q') | KeyCode::Char('Q')
+                if key_event.modifiers == KeyModifiers::CONTROL =>
+            {
+                self.exit()
+            }
+            KeyCode::Char('r') | KeyCode::Char('R')
+                if key_event.modifiers == KeyModifiers::CONTROL =>
+            {
+                self.choose_random_word()
+                    .expect("Could not reload random word!");
+            }
+            KeyCode::Char('v') | KeyCode::Char('V')
+                if key_event.modifiers == KeyModifiers::CONTROL =>
+            {
+                self.reveal_answer = !self.reveal_answer
+            }
+            _ => (),
+        };
+
+        Ok(())
     }
 
     fn exit(&mut self) {
         self.exit = true;
     }
-
-    fn add_to_counter(&mut self, interval: i64) {
-        self.counter += interval as i64;
-    }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App Tutorial ".bold());
+        let title = Title::from(" Worlde TUI in Rust ".bold());
         let instruction = Title::from(Line::from(vec![
-            " Decrement by 1 ".blue(),
-            "<j>".blue().bold(),
-            " Decrement by 10 ".blue(),
-            "<J>".blue().bold(),
-            " Increment by 10 ".blue(),
-            "<K>".blue().bold(),
-            " Increment by 1 ".blue(),
-            "<k>".blue().bold(),
-            " Quit ".blue(),
-            "<Q> ".blue().bold(),
+            " <Ctrl-r> to refresh word ".blue(),
+            " <Ctrl-v> to reveal word ".blue(),
+            " <Ctrl-q> to exit ".blue(),
         ]));
 
-        let block = Block::bordered()
+        let top_block = Block::bordered()
             .border_style(Style::default().blue())
             .title(title.alignment(Alignment::Center))
             .title(
@@ -93,31 +129,27 @@ impl Widget for &App {
                     .alignment(Alignment::Center)
                     .position(Position::Bottom),
             )
+            .padding(Padding::vertical(2))
             .border_set(border::THICK);
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
+        let current_word_text = Text::from(vec![Line::from(vec![if self.reveal_answer {
+            self.target_word.as_str().red().underlined()
+        } else {
+            "".into()
+        }])]);
 
-        Paragraph::new(counter_text)
+        Paragraph::new(current_word_text)
             .centered()
-            .block(block)
-            .render(area, buf)
+            .block(top_block)
+            .render(area, buf);
     }
 }
 
 fn main() -> Result<()> {
-    color_eyre::install()?;
     let mut terminal = tui::init()?;
     // App::default() will init app with counter set to 0, and exit set to false
-    let app_result = App::default().run(&mut terminal);
-    if let Err(err) = tui::restore() {
-        eprintln!(
-            "Fialed to restore terminal. Run reset, or restart terimnal to fix.\n{}",
-            err
-        )
-    }
+    let _ = App::default().run(&mut terminal);
+    let _ = tui::restore();
 
-    app_result
+    Ok(())
 }
